73e3cba80526bc2667e68dbf77e0e4007fc80649
Cranium1 <carson@ccrane.me>
2015-10-03 13:06:32 -0400

    Add routing

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
-var BlogApp = angular.module('BlogApp', []);
+var DemoApp = angular.module('DemoApp', ['ngRoute']);
 
-BlogApp.controller('PreviewController', function($scope) {
+DemoApp.config(function($routeProvider, $locationProvider) {
+	$routeProvider
+    .when('/', {
+        templateUrl : 'markdown.html',
+        controller  : 'MarkdownController',
+    })
+	.when('/markdown', {
+		templateUrl : 'markdown.html',
+		controller  : 'MarkdownController'
+	})
+	.when('/ransom', {
+		templateUrl : 'ransom.html',
+		controller  : 'RansomController'
+	});
+
+	// $locationProvider.html5Mode(true);
+});
+
+DemoApp.controller('RansomController', function($scope) {
+	$scope.$watch('input', function(val) {
+
+		var ransomstring = "";
+		for (var i = 0, len = val.length; i < len; i++) {
+			if (val[i].match(/[a-z]/i)) {
+				ransomstring = ransomstring.concat("<img src='/ransomimg/"+val[i]+".png'>");
+			}
+		}
+
+		$scope.output = ransomstring;
+	}, true);
+
+	$scope.input = ""
+	$scope.output = ""
+});
+
+
+DemoApp.controller('MarkdownController', function($scope) {
 	$scope.$watch('input', function(val) {
 		$scope.output = markdown.toHTML(val);
 	}, true);
@@ -9,4 +45,4 @@ BlogApp.controller('PreviewController', function($scope) {
 	$scope.output = ""
 });
 
-BlogApp.filter('html', function($sce) { return $sce.trustAsHtml; });
+DemoApp.filter('html', function($sce) { return $sce.trustAsHtml; });
